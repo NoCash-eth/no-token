3290449b1f7b8705e804af5489068104d5b52f41
codingsh <codingsh@pm.me>
2020-10-18 12:36:24 +0000

    ERC20 NoToken is Token for Nocash.digital

---
+++ README.md
@@ -0,0 +1,8 @@
+# NoToken is a Nocash.digital token
+NoToken erc20 token contract
+
+code base is https://github.com/dego-labs/dego-token
+
+### Partner brands
+[Gitcoin](gitcoin.co)
+[cartesi](https://cartesi.io/)
---
+++ scripts/head.sol
@@ -0,0 +1,35 @@
+/****
+*
+* 888b      88                 ,ad8888ba,                          88
+* 8888b     88                d8"'    `"8b                         88
+* 88 `8b    88               d8'                                   88
+* 88  `8b   88   ,adPPYba,   88             ,adPPYYba,  ,adPPYba,  88,dPPYba,
+* 88   `8b  88  a8"     "8a  88             ""     `Y8  I8[    ""  88P'    "8a
+* 88    `8b 88  8b       d8  Y8,            ,adPPPPP88   `"Y8ba,   88       88
+* 88     `8888  "8a,   ,a8"   Y8a.    .a8P  88,    ,88  aa    ]8I  88       88
+* 88      `888   `"YbbdP"'     `"Y8888Y"'   `"8bbdP"Y8  `"YbbdP"'  88       88
+*
+* https://nocash.digital
+*
+* MIT License
+* ===========
+*
+* Copyright (c) 2020 NoCash
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*/
